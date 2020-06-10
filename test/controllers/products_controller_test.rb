022a9d652809166e755c7f162ff84ea668876d93
ChelseaC13 <chelhelg@outlook.com>
2020-06-10 15:36:23 -0700

    index test done

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 describe ProductsController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "index" do
+    it "succeeds when there are products" do
+      get products_path
+      must_respond_with :success
+    end
+
+    it "succeeds when there are no products" do
+      Product.all do |product|
+        product.destroy
+      end
+
+      get products_path
+
+      must_respond_with :success
+    end
+  end
 end
