aa6302bd9678de8e2889bd2bd4901d19301b36a7
Faezeh Ashtiani <faezeh.ashtiani@gmail.com>
2020-06-10 15:36:07 -0700

    finalized products partial and by category view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,6 +6,11 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def category_products
+    @category = Category.find_by_id(params[:category_id])
+    @category_products = @category.products #.sort{ |a, b| b.average_rating <=> a.average_rating }
+  end
+
   def new
     @product = Product.new
   end 
@@ -38,8 +43,5 @@ class ProductsController < ApplicationController
   def new_product_params 
     return params.require(:product).permit(:name, :price, :img_url, :description, :qty)
   end 
-  def category_products
-    category = Category.find_by_id(params[:category_id])
-    @category_products = category.products #.sort{ |a, b| b.average_rating <=> a.average_rating }
-  end
+  
 end
