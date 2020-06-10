f00e707652fe0750031023ea27cd8e2cbe8ecc70
quinqu <quinruby0@gmail.com>
2020-06-10 10:48:39 -0700

    o-auth

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,13 @@
 class Merchant < ApplicationRecord
   has_many :products 
+
+  def self.create_merchant_from_github(auth_hash)
+    merchant = Merchant.new
+    merchant.uid = auth_hash["uid"]
+    merchant.provider = auth_hash["provider"]
+    merchant.username = auth_hash["info"]["name"]
+    merchant.email = auth_hash["info"]["email"]
+
+    return merchant 
+  end 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_09_232403) do
+ActiveRecord::Schema.define(version: 2020_06_10_174232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 2020_06_09_232403) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "uid"
+    t.string "provider"
   end
 
   create_table "order_items", force: :cascade do |t|
