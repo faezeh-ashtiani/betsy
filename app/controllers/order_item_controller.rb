b3f84cb68badbe8155ad00e11e444c638c79afc0
quinqu <quinruby0@gmail.com>
2020-06-11 12:43:08 -0700

    added checkout form

--- app/controllers/order_item_controller.rb
+++ app/controllers/order_item_controller.rb
@@ -11,16 +11,13 @@ class OrderItemController < ApplicationController
   end 
 
   def cart
-    puts session[:order_items]
     @cart = [] 
     session[:order_items].each do |order_item|
-        puts "THIS IS ORSER IDEM " + order_item.to_s
         product = Product.find_by(id: order_item["product_id"])
       if !product.nil?
         @cart << product.name
       end 
     end
-    puts "This is cart" + @cart.to_s
     return @cart
   end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,17 +6,20 @@ Rails.application.routes.draw do
   get "/auth/github", as: "github_login"
   get "/auth/:provider/callback", to: "merchants#create"
 
-  #resources :merchants
+  resources :merchants
   resources :products
-  #resources :carts
-  #resources :order_items
-  resources :categories 
 
+  resources :categories 
+  #quin ---- 
   get '/categories/:category_id/products', to: 'products#category_products', as: 'category_products'
-  post 'order/check-out', to: 'order#check_out', as: 'check_out'
+
+  get 'orders/new', to: "orders#new", as: 'new_order'
+  
+  post 'orders/check-out', to: 'orders#check_out', as: 'check_out'
 
   get 'guest/cart', to: 'order_item#cart', as: 'cart'
   post 'products/:id/add-to-cart', to: 'order_item#add_to_cart', as: 'add_to_cart'
+  #----
   
   
   resources :reviews
