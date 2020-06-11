11a2a08b37770abff34f9da18b83bad58f380adb
quinqu <quinruby0@gmail.com>
2020-06-10 20:50:53 -0700

    order items

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,2 +1,14 @@
 class OrderItemsController < ApplicationController
+
+  def add_to_cart
+    order = Order.find_by(id: session[:order_id])
+    if session[:order_id].nil?
+      order = Order.create()
+      session[:order_id] = order.id
+    end 
+      
+    
+
+  end 
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,13 @@
 class Order < ApplicationRecord
   has_many :order_items
   has_many :products, through: :order_items
+
+
+
+  def check_out
+    #call a method here that goes through all the order items and updates quantity 
+
+  end 
+
+
 end
