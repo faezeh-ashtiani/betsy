a8154341f949e413fd7f5f225eeae7737afab182
quinqu <quinruby0@gmail.com>
2020-06-11 11:23:09 -0700

    cart page

--- app/controllers/order_item_controller.rb
+++ app/controllers/order_item_controller.rb
@@ -11,4 +11,18 @@ class OrderItemController < ApplicationController
 
     redirect_to product_path(params[:id])
   end 
+
+  def cart
+    @cart = [] 
+
+    session[:order_items].each do |order_item|
+      
+        product = Product.find_by(id: order_item[:product_id])
+      if !product.nil?
+        @cart << product.name
+      end 
+    end
+
+    return @cart
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,14 +8,16 @@ Rails.application.routes.draw do
 
   #resources :merchants
   resources :products
-  resources :carts
-  resources :order_items
+  #resources :carts
+  #resources :order_items
   resources :categories 
 
   get '/categories/:category_id/products', to: 'products#category_products', as: 'category_products'
-
   post 'order/check-out', to: 'order#check_out', as: 'check_out'
+
+  get 'guest/cart', to: 'order_item#cart', as: 'cart'
   post 'products/:id/add-to-cart', to: 'order_item#add_to_cart', as: 'add_to_cart'
+  
   
   resources :reviews
   
