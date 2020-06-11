c1a35960c24b0d38083c28af3deb544c91b63b0b
quinqu <quinruby0@gmail.com>
2020-06-10 21:21:52 -0700

    fixed schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_11_033959) do
+ActiveRecord::Schema.define(version: 2020_06_11_040334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2020_06_11_033959) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "product_id"
+    t.integer "qty"
     t.index ["product_id"], name: "index_order_items_on_product_id"
   end
 
