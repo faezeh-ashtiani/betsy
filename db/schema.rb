c82a19a720ec3dad9689d8dc44250d01c2a85c72
quinqu <quinruby0@gmail.com>
2020-06-10 20:56:37 -0700

    migrations for order items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_10_174232) do
+ActiveRecord::Schema.define(version: 2020_06_11_035535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_06_10_174232) do
   create_table "order_items", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "qty"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -47,7 +48,6 @@ ActiveRecord::Schema.define(version: 2020_06_10_174232) do
     t.string "credit_card"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "qty"
   end
 
   create_table "products", force: :cascade do |t|
