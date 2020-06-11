76f32615a37f59d47727a41d52bea5b513689066
quinqu <quinruby0@gmail.com>
2020-06-11 11:32:48 -0700

    tried displaying products

--- app/controllers/order_item_controller.rb
+++ app/controllers/order_item_controller.rb
@@ -2,21 +2,22 @@ class OrderItemController < ApplicationController
 
   def add_to_cart   
     if session[:order_items] 
-      session[:order_items] << OrderItem.create(qty: params[:qty], product_id: params[:id])
+      session[:order_items] << OrderItem.create(qty: params[:post][:qty], product_id: params[:id])
     else
       session[:order_items] = []
       session[:order_items] << OrderItem.create(qty: params[:post][:qty], product_id: params[:id])
     end 
-
-
+    
+    session[:order_items].each do |item|
+      product = Product.find_by(id: item[:product_id])
+      puts product
+    end 
     redirect_to product_path(params[:id])
   end 
 
   def cart
     @cart = [] 
-
     session[:order_items].each do |order_item|
-      
         product = Product.find_by(id: order_item[:product_id])
       if !product.nil?
         @cart << product.name
