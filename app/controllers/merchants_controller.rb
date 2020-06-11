d23f8db68b45abfb49e6d3380cfe21ba0967ee18
quinqu <quinruby0@gmail.com>
2020-06-11 10:12:59 -0700

    add to cart functionality

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -19,5 +19,4 @@ class MerchantsController < ApplicationController
     session[:user_id] = user.id
     redirect_to root_path
   end
-
 end
--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,13 +1,16 @@
 class OrderItemsController < ApplicationController
 
   def add_to_cart
-    order = Order.find_by(id: session[:order_id])
-    if session[:order_id].nil?
-      order = Order.create()
-      session[:order_id] = order.id
-    end 
+    # order = Order.find_by(id: session[:order_id])
+    # if session[:order_id].nil?
+    #   order = Order.create()
+    #   session[:order_id] = order.id
+    # end 
+
+    session[:order_items] << OrderItems.create(qty: params[:qty], product_id: @product.id)
       
     
+    order_items = OrderItems(qty: params[:qty], product_id: @product.id)
 
   end 
 
