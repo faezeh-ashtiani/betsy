8c8d4ed25a3efa1f9419df72bc3eeeaee5a5c725
quinqu <quinruby0@gmail.com>
2020-06-11 11:47:27 -0700

    products can be shown in cart

--- app/controllers/order_item_controller.rb
+++ app/controllers/order_item_controller.rb
@@ -7,23 +7,20 @@ class OrderItemController < ApplicationController
       session[:order_items] = []
       session[:order_items] << OrderItem.create(qty: params[:post][:qty], product_id: params[:id])
     end 
-    
-    session[:order_items].each do |item|
-      product = Product.find_by(id: item[:product_id])
-      puts product
-    end 
     redirect_to product_path(params[:id])
   end 
 
   def cart
+    puts session[:order_items]
     @cart = [] 
     session[:order_items].each do |order_item|
-        product = Product.find_by(id: order_item[:product_id])
+        puts "THIS IS ORSER IDEM " + order_item.to_s
+        product = Product.find_by(id: order_item["product_id"])
       if !product.nil?
         @cart << product.name
       end 
     end
-
+    puts "This is cart" + @cart.to_s
     return @cart
   end 
 end
