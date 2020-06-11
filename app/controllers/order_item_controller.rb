3b2f688c49f2a5e61d41502a9c73f7b3f7327aab
quinqu <quinruby0@gmail.com>
2020-06-11 15:06:29 -0700

    tried remove item functionality

--- app/controllers/order_item_controller.rb
+++ app/controllers/order_item_controller.rb
@@ -10,11 +10,18 @@ class OrderItemController < ApplicationController
     redirect_to product_path(params[:id])
   end 
 
+  def remove_from_cart 
+    session[:order_items] = OrderItem.remove_from_cart(session["order_items"], params["format"].to_i)
+    #THIS IS PARAMS: {"_method"=>"delete", "authenticity_token"=>"xxp4QJVjqTNKxmdNcR6GlEz4S1fN5m54uplkVFD4sEBFKL/K81Rfextg5H0P3CXm57eZ1CRNcdqPwMESDSG77w==", "controller"=>"order_item", "action"=>"remove_from_cart", "format"=>"8"}
+    puts params
+  end 
+
+
+
   def cart 
     if session[:order_items].nil? 
       redirect_to root_path
     else
-
       @cart = OrderItem.unique_items(session[:order_items])
       return @cart
     end 
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -5,7 +5,7 @@ class OrderItem < ApplicationRecord
       unique_items = {}
       
       order_items.each do |item|
-        puts "THISSS item #{item}"
+      
         qty = item["qty"]
 
         product = Product.find_by(id: item["product_id"])
@@ -16,8 +16,20 @@ class OrderItem < ApplicationRecord
           unique_items[product] = qty
         end 
       end 
-      puts "THIS IS UNIQUE ITEMS : " + unique_items.to_s
+      
       return unique_items 
+    end 
+
+    def self.remove_from_cart(session, product_id)
+      updated_cart =  []
+      puts "THIS IS PRODUCT ID #{product_id}"
+      session.each do |item|
+        if product_id != item["product_id"]
+          updated_cart << item 
+        end 
+      end
+      puts "THIS IS SESSION 3 #{updated_cart}"
+      return updated_cart
     end 
   
 end
