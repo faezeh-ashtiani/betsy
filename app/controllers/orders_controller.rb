31579ffaa384e74c3d13f4f71f12f8e0f616ebd2
quinqu <quinruby0@gmail.com>
2020-06-11 13:17:20 -0700

    added cart checkout

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -17,6 +17,7 @@ class OrdersController < ApplicationController
 
   def check_out 
 
+    
 
   end 
 
--- config/routes.rb
+++ config/routes.rb
@@ -8,15 +8,12 @@ Rails.application.routes.draw do
 
   resources :merchants
   resources :products
-
+  resources :orders, only: [:new, :create]
   resources :categories 
   #quin ---- 
   get '/categories/:category_id/products', to: 'products#category_products', as: 'category_products'
 
-  get 'orders/new', to: "orders#new", as: 'new_order'
-
-
-  post 'orders/check-out', to: 'orders#create', as: 'check_out'
+  
 
   get 'guest/cart', to: 'order_item#cart', as: 'cart'
   post 'products/:id/add-to-cart', to: 'order_item#add_to_cart', as: 'add_to_cart'
