dddc6601449c5c2be73be59ed6637f738b9f60cf
Faezeh Ashtiani <faezeh.ashtiani@gmail.com>
2020-06-10 17:19:01 -0700

    added controller test for products#category_products

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -17,4 +17,20 @@ describe ProductsController do
       must_respond_with :success
     end
   end
+
+  describe "category_products" do
+    it "succeeds when there are products in a category" do
+      category_id = categories(:category1).id
+      get category_products_path(category_id)
+      must_respond_with :success
+    end
+
+    it "succeeds when there are no products" do
+      Product.destroy_all
+      category_id = categories(:category1).id
+      get category_products_path(category_id)
+
+      must_respond_with :success
+    end
+  end
 end
