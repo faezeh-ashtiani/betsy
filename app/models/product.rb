27fa5658bda4d33ad4744aba9df2aab6a4a1732a
ChelseaC13 <chelhelg@outlook.com>
2020-06-11 15:17:57 -0700

    want to catch my branch with master so i can finish merchant

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,9 +3,7 @@ class Product < ApplicationRecord
   has_many :order_items
   has_and_belongs_to_many :categories
   has_many :orders, through: :order_items
-  # belongs_to :merchant
-
-
+  belongs_to :merchant
 
   protected 
 
