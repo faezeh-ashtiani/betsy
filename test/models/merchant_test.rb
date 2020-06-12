5df8c401b7f94d162f2333175fa2cc21d39b7f83
ChelseaC13 <chelhelg@outlook.com>
2020-06-11 20:38:03 -0700

    who finally finished these relationship test... me

--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -1,21 +1,20 @@
 require "test_helper"
 
 describe Merchant do
-  # describe "merchant product relations" do
-  #   let(:merchant) { products(:merchant1) }
+  describe "merchant product relations" do
+    let(:merchant) { merchants(:merchant1) }
   
     
-  #   it "can have many products" do
-  #     product1 = products(:product1)
-  #     product2 = products(:product2)
-
-  #     expect(merchant.products.length).must_equal 2
-  #   end
+    it "can have many products" do
+      product1 = Product.create(name: "mask", price: 2.99, img_url: "www.pic.com", description: "this is a description", qty: 10, merchant_id: merchant.id)
+      product2 = Product.create(name: "marker", price: 1.00, img_url: "www.marker.com", description: "better description", qty: 10, merchant_id: merchant.id)
+      expect(merchant.products.length).must_equal 2
+    end
     
-  #   it "can have zero products" do
-  #     product2 = products(:product2)
+    it "can have zero products" do
+      merchant2 = merchants(:merchant2)
       
-  #     expect(merchant.products.length).must_equal 0
-  #   end
-  # end
+      expect(merchant2.products.length).must_equal 0
+    end
+  end
 end
