c1a854f9f33f516ff5a4d28a03b72cefc3640ccc
quinqu <quinruby0@gmail.com>
2020-06-13 12:24:30 -0700

    some controller testing

--- test/controllers/order_item_controller_test.rb
+++ test/controllers/order_item_controller_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 describe OrderItemController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "add to cart" do 
+    it "will add product to empty cart!" do 
+      product = products(:product1)
+
+      par = {
+        id: 1, 
+        post: { qty: 2 }
+      }
+      post add_to_cart_path(product.id), params: par
+      get cart_path 
+
+      expect(@cart).nil?.must_equal false
+    end 
+  end 
+
+
+  describe "remove from cart" do 
+
+
+  end 
 end
