b2c8b45c43f419eeeb4b0fc28ee67732535c2d06
Faezeh Ashtiani <faezeh.ashtiani@gmail.com>
2020-06-12 22:24:05 -0700

    category validation and test - not completely working

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
   has_and_belongs_to_many :products
-
-
+  validates :name, presence: true, uniqueness: true
+  # validates :product_id, presence: true, numericality: { only_integer: true }
 
   protected 
 
