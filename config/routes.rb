960c6f82a40ec467f0f9f0cd1aedb5eae62f7ba4
quinqu <quinruby0@gmail.com>
2020-06-14 21:02:05 -0700

    styling button

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,6 @@ Rails.application.routes.draw do
 
 
   get 'guest/cart', to: 'order_item#cart', as: 'cart'
-  post 'order/check-out', to: 'order#check_out', as: 'check_out'
   post 'products/:id/add-to-cart', to: 'order_item#add_to_cart', as: 'add_to_cart'  
  
   post 'guest/cart/remove', to: 'order_item#remove_from_cart', as: 'remove_from_cart'
