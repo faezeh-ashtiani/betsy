c65e59b2a68814c9c58e080aed5fa070863613b7
ChelseaC13 <chelhelg@outlook.com>
2020-06-16 21:36:17 -0700

    adding one more of the same item to cart increases quantity

--- app/controllers/order_item_controller.rb
+++ app/controllers/order_item_controller.rb
@@ -9,12 +9,25 @@ class OrderItemController < ApplicationController
 
     if session[:order_items] 
       item = OrderItem.create!(qty: (params[:post][:qty]).to_i, product_id: params[:id])
-      if item.qty < 1
+      if item.qty < 1 #TODO write test for this
         flash[:error] = "Before adding to cart please specify a quantity"
         return redirect_to root_path
       end
-      session[:order_items] << OrderItem.create!(qty: (params[:post][:qty]).to_i, product_id: params[:id])
-      flash[:status] = "Added to Cart!"
+    
+      foundCartEntry = false
+      
+      session[:order_items].each do |order_item|
+        if order_item.key(item.product_id)
+          foundCartEntry = true
+          order_item["qty"] += item.qty
+          flash[:status] = "Added to Cart!"
+        end
+      end
+
+      if !foundCartEntry
+        session[:order_items] << OrderItem.create!(qty: (params[:post][:qty]).to_i, product_id: params[:id])
+        flash[:status] = "Added to Cart!"
+      end
     else
       session[:order_items] = []
       session[:order_items] << OrderItem.create!(qty: params[:post][:qty], product_id: params[:id])
