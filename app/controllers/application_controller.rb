9978a9b9c01ce91cab470c12e59324ffafb079f0
Faezeh Ashtiani <faezeh.ashtiani@gmail.com>
2020-06-18 14:53:21 -0700

    added blacl color to all links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :current_merchant
 
   def current_merchant
-    if session[:user_id]
-      @current_merchant = Merchant.find_by(id: session[:user_id])
-    end
+    @current_merchant = Merchant.find_by(id: 1)
+    # if session[:user_id]
+    #   @current_merchant = Merchant.find_by(id: session[:user_id])
+    # end
   end
 
   def require_login
